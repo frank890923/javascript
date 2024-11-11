cc0c37cf2411d51dbd1cd30c34c10194aa820661
frl2389 <fpipicanocorrea@gmail.com>
2024-11-11 02:10:21 -0500

    pokemon

--- javascript/JS/fetch.js
+++ javascript/JS/fetch.js
@@ -3,8 +3,15 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then((data)=>{
 
     console.log(data);
-    document.write(data.id);
+    document.write(data[0].name);
+    datospintados(data);
 });
+
+const datospintados = (datos) => {
+    for( i=0; i<=datos.length; i++){
+        document.write("<p>"+datos[i].name+"</p>");
+    }
+};
 
 
 
